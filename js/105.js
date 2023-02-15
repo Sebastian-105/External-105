c9670d6e9d98a3dabe59c48aff2dc96db1db664e
Sebastian-105 <83544451+Sebastian-105@users.noreply.github.com>
2023-02-15 22:32:13 +0000

    icon

--- js/105.js
+++ js/105.js
@@ -1,1 +1,31 @@
 
+function sebastian() {
+
+  const icon = `
+  <style>
+  #badge {
+    position: fixed;
+    cursor: pointer;
+    bottom: 16px;
+    right: 16px;
+    z-index: 1000;
+  }
+
+  #badge img{
+    opacity: 0.5;
+    transition-property: opacity, transform;
+    transition: opacity 120ms, transform 120ms;
+    transition-timing-function: ease-out;
+  }
+  #badge:hover img {
+    transform: scale(1.05);
+    opacity: 1;
+  }
+  </style>
+  <a id="badge" href="/index.html"><img src="../105.png" alt="105" title="Sebastian-105"></a>
+    `;
+
+  document.body.insertAdjacentHTML('beforeend', icon);
+}
+
+sebastian();

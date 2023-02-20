50a537e1cd029a9651097787e11f08417f38233d
Sebastian-105 <83544451+Sebastian-105@users.noreply.github.com>
2023-02-19 19:12:41 -0500

    Create Sebastian105.js

---
+++ Sebastian105.js
@@ -0,0 +1,31 @@
+
+function sebastian() {
+
+  const icon = `
+  <style>
+  #badge {
+    position: fixed;
+    cursor: pointer;
+    bottom: 16px;
+    left: 16px;
+    z-index: 1000;
+  }
+
+  #badge img{
+    opacity: 0.5;
+    transition-property: opacity, transform;
+    transition: opacity 120ms, transform 120ms;
+    transition-timing-function: ease-out;
+  }
+  #badge:hover img {
+    transform: scale(1.05);
+    opacity: 1;
+  }
+  </style>
+  <a id="badge" href="/index.html"><img src="https://icon105.netlify.app/105.round.png" style="width:50px;height:50px;" alt="105" title="Sebastian-105"></a>
+    `;
+
+  document.body.insertAdjacentHTML('beforeend', icon);
+}
+
+sebastian();

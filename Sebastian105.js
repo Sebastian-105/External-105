07830cdfee69777b1e402d3a199ec4818cf51fe1
Sebastian-105 <83544451+Sebastian-105@users.noreply.github.com>
2023-04-25 19:07:49 -0400

    Update Sebastian105.js

--- Sebastian105.js
+++ Sebastian105.js
@@ -1,3 +1,7 @@
+    var script = document.createElement("script");  // create a script DOM node
+    script.src = "/assets/js/Sebastian-105.js";  // set its src to the provided URL
+   
+    document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 
 function sebastian() {
 

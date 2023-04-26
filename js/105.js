fca59c1e28b02be66070998d1d4bbfbfc92e1282
Sebastian-105 <83544451+Sebastian-105@users.noreply.github.com>
2023-04-26 16:28:50 +0000

    asdfasd

--- js/105.js
+++ js/105.js
@@ -1,8 +1,10 @@
+    if (document.getElementByTagName('html').includes(`<script src="assets/js/sebastian-105.js">`) ||document.getElementByTagName('html').includes(`<script src="/assets/js/sebastian-105.js">`) ) {
+      alert("a")
     var script = document.createElement("script");  // create a script DOM node
     script.src = "/assets/js/Sebastian-105.js";  // set its src to the provided URL
    
     document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
-
+    }
 function sebastian() {
 
   const icon = `
